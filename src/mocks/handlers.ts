ef3fb8f95bb38aac8bc703c99817c00eee571bd4
kolya-zalev <kolaov356@email.com>
2026-05-20 09:46:40 +0300

    GET id

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,8 +1,19 @@
 import { http, HttpResponse } from "msw";
 import { fakeLeads } from "@/mocks/fakedata";
 
+let leadsDb = [...fakeLeads];
+
 export const handlers = [
   http.get("/api/leads", () => {
-    return HttpResponse.json([...fakeLeads]);
+    return HttpResponse.json(leadsDb);
+  }),
+
+  http.get<{ id: string }>("/api/leads/:id", ({ params }) => {
+    const { id } = params;
+    const foundLead = leadsDb.find((lead) => lead.id === id);
+    if (!foundLead) {
+      return HttpResponse.json({ error: "Not found" }, { status: 404 });
+    }
+    return HttpResponse.json(foundLead);
   }),
 ];
