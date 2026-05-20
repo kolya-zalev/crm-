e7c54c28db294e0958a8533fa95fd666fd1ce1e6
kolya-zalev <kolaov356@email.com>
2026-05-20 17:08:06 +0300

    table

--- src/app/leads/page.tsx
+++ src/app/leads/page.tsx
@@ -1,3 +1,5 @@
-export default function LeadsPage () {
-    return <div>Leads</div>
+import { TableDemo } from "@/components/leads-comp/LeadsTable/LeadsTable.component";
+
+export default function LeadsPage() {
+  return <TableDemo></TableDemo>;
 }
