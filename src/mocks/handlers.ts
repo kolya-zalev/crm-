1eceeeb11f106b8e1c51cee6b2d9448cddac2996
kolya-zalev <kolaov356@email.com>
2026-05-20 10:28:44 +0300

    post

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,5 +1,6 @@
 import { http, HttpResponse } from "msw";
 import { fakeLeads } from "@/mocks/fakedata";
+import { Lead } from "@/lib/types/lead";
 
 let leadsDb = [...fakeLeads];
 
@@ -12,8 +13,25 @@ export const handlers = [
     const { id } = params;
     const foundLead = leadsDb.find((lead) => lead.id === id);
     if (!foundLead) {
-      return HttpResponse.json({ error: "Not found" }, { status: 404 });
+      return HttpResponse.json(
+        {
+          error: {
+            message: "Lead not Found",
+            code: "NOT_FOUND",
+          },
+        },
+        { status: 404 },
+      );
     }
     return HttpResponse.json(foundLead);
   }),
+
+  http.post("/api/leads", async ({ request }) => {
+    const body = (await request.json()) as Omit<Lead, "id">;
+    const newLead: Lead = { ...body, id: crypto.randomUUID() };
+    leadsDb.push(newLead);
+    return HttpResponse.json(newLead, { status: 201 });
+  }),
+
+
 ];
