3b3081496eea820a9a6fa96afc04263f6aa4f6e9
kolya-zalev <kolaov356@email.com>
2026-05-29 22:28:26 +0300

    feat: add dark mode in the section 'settings'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import { AppSidebar } from "@/components/layout/AppSidebar";
 import Navbar from "@/components/layout/Navbar";
 import { MSWProvider } from "@/mocks/MswProvider";
 import { Toaster } from "sonner";
+import { ThemeProvider } from "@/components/ThemeProvider/theme-provider";
 
 
 const jetbrainsMono = JetBrains_Mono({
@@ -39,6 +40,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
+      suppressHydrationWarning
       className={cn(
         "h-full",
         "antialiased",
@@ -48,7 +50,9 @@ export default function RootLayout({
         jetbrainsMono.variable,
       )}
     >
+      
       <body className="h-full flex flex-col overflow-hidden">
+        <ThemeProvider attribute='class' defaultTheme="system" enableSystem>
         <MSWProvider>
         <Navbar />
         <div className="flex flex-1 overflow-hidden">
@@ -57,9 +61,14 @@ export default function RootLayout({
             <main className="flex-1 overflow-y-auto p-6">{children}</main>
           </SidebarProvider>
         </div>
+        
         </MSWProvider>
+        
         <Toaster position="top-center" richColors />
+        </ThemeProvider>
       </body>
+      
+      
     </html>
   );
 }
